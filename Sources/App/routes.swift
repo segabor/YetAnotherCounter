8f86fa4a3667a87b08ae73f9f5dacb7c55e007aa
Gabor SEBESTYEN <segabor@gmail.com>
2019-12-03 18:07:07 +0100

    Add logging support

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Logging
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -12,12 +13,19 @@ public func routes(_ router: Router) throws {
 
     // state getter
     router.get("counter") { req -> Counter in
+        let logger = try req.make(Logger.self)
+
+        logger.debug("Returning state \(state)")
+
         return state
     }
 
     // state setter
     router.post("counter") { req -> HTTPStatus in
+        let logger = try req.make(Logger.self)
         let nextState = try req.content.syncDecode(Counter.self)
+
+        logger.debug("Change state \(state) to \(nextState)")
 
         state = nextState
 
